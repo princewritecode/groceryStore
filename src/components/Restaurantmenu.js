c79102ce59215ecd1f04b57cdcb3eb6199c16987
princewritecode <princewritecode@gmail.com>
2024-06-13 00:22:08 +0530

    food application version 1.1

--- src/components/Restaurantmenu.js
+++ src/components/Restaurantmenu.js
@@ -24,7 +24,7 @@ const RestaurantMenu = () =>
     if (resInfo === null) return <Shimmer></Shimmer>;
 
     const { name, cuisines } = resInfo.cards[2].card.card.info;
-    const { itemCards } = resInfo.cards[5].groupedCard.cardGroupMap.REGULAR.cards[2].card.card;
+    const { itemCards } = resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[2].card.card;
     return (<div className="menu">
         <h1>{name}</h1>
         <img></img>
